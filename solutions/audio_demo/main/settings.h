dc609e3e6cac7c10666cf556c7ed9bc2b4eba367
João Barioni <joao.barioni.2002@gmail.com>
2025-05-20 19:06:43 -0300

    Refactor WiFi and MQTT configuration to use dynamic settings

--- solutions/audio_demo/main/settings.h
+++ solutions/audio_demo/main/settings.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#define WIFI_SSID "MINDTECH"
-#define WIFI_PASSWORD "Mindtech@2023#"
+#define WIFI_SSID CONFIG_WIFI_STATION_SSID
+#define WIFI_PASSWORD CONFIG_WIFI_STATION_PASSWORD
 
-#define MQTT_URL "mqtt://mqtt.dev.ppacontatto.com.br"
-#define MQTT_USER "mateus"
-#define MQTT_PASS "a111276A"
+#define MQTT_URL CONFIG_MQTT_BROKER 
+#define MQTT_USER CONFIG_MQTT_USER
+#define MQTT_PASS CONFIG_MQTT_PASS
